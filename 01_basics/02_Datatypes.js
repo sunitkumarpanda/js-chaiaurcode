c620e57d0eb390b38398c16c7c8538f7105b2578
Sunit Ku. Panda <115678679+Sunitpanda0124@users.noreply.github.com>
2024-06-01 22:33:43 +0530

    Update 02_Datatypes.js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) // we are usig nodejs, not browser
+
+console.log(3 + 3);
+
+// code readability should be high
+
+console.log("Sunit")
+
+
+let name = "sunit"
+let age = 19
+let isLoggedIn = true
+
+
+// number => 2 to power of 53
