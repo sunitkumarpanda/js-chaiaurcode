bec7512096009eae593c318a16c8df0141bcdeda
Sunit Ku. Panda <115678679+Sunitpanda0124@users.noreply.github.com>
2024-06-01 23:22:04 +0530

    Create 03_conversionOperations.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "Sunit"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "sunit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sunit" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
